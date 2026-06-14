cbcfb65bfaf8677af8655e884b637484af5bdef5
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2026-06-14 13:55:48 +0200

    added a class instead of functions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Application
 import getpass
-from lib.crud import create_task, view_tasks, complete_task, delete_task, update_task
+from lib.crud import Task
 
 exit = False
 username = getpass.getuser()
@@ -19,24 +19,24 @@ while exit == False:
     input_choice = input("Please enter your choice (1-5): ")
     if input_choice == "1":
         task_name = input("Enter the task name: ")
-        create_task(task_name)
+        Task.create_task(task_name)
         print(f"Task '{task_name}' created successfully!")
     elif  input_choice == "2":
         print("-----------------------------------------------------------------------")
-        view_tasks()
+        Task.view_tasks()
     elif input_choice == "3":
-        view_tasks()
+        Task.view_tasks()
         task_number = input("Enter the task ID to complete: ")
-        complete_task(int(task_number))
+        Task.complete_task(int(task_number))
     elif input_choice == "4":
+        Task.view_tasks()
         task_number = input("Enter the task ID to delete: ")
-        delete_task(int(task_number))
-        print(f"Task ID '{task_number}' deleted successfully!")
+        Task.delete_task(int(task_number))
     elif input_choice == "5":
-        view_tasks()
+        Task.view_tasks()
         task_number = input("Enter the task ID to edit: ")
         new_task_name = input("Enter the new task name: ")
-        update_task(int(task_number), new_task_name)
+        Task.update_task(int(task_number), new_task_name)
     else:
         print("Exiting the application. Goodbye!")
         exit = exit = True
